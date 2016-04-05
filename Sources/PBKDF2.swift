3990f7cf4d28768567d582ee4a6500412d9f327c
Joannis Orlandos <joannis@orlandos.nl>
2016-04-05 17:09:39 +0200

    Passwords as Bytes are now supported

--- Sources/PBKDF2.swift
+++ Sources/PBKDF2.swift
@@ -12,15 +12,12 @@ import CryptoEssentials
 
 public final class PBKDF2 {
     /// Used for applying an HMAC variant on a password and salt
-    private static func digest(password: String, data: [Byte], variant: HashProtocol.Type) throws -> [Byte] {
-        var passwordBytes = [Byte]()
-        passwordBytes.append(contentsOf: password.utf8)
-        
-        return HMAC.authenticate(key: passwordBytes, message: data, variant: variant)
+    private static func digest(password: [Byte], data: [Byte], variant: HashProtocol.Type) throws -> [Byte] {
+        return HMAC.authenticate(key: password, message: data, variant: variant)
     }
     
     /// Applies the `hi` (PBKDF2 with HMAC as PseudoRandom Function)
-    public static func calculate(password: String, salt: [Byte], iterations: Int, variant: HashProtocol.Type) throws -> [Byte] {
+    public static func calculate(password: [Byte], salt: [Byte], iterations: Int, variant: HashProtocol.Type) throws -> [Byte] {
         var salt = salt
         salt.append(contentsOf:)(contentsOf: [0, 0, 0, 1])
         
@@ -29,6 +26,25 @@ public final class PBKDF2 {
         
         for _ in 0..<iterations - 1 {
             u1 = try digest(password, data: u1, variant: variant)
+            ui = xor(ui, u1)
+        }
+        
+        return ui
+    }
+    
+    /// Applies the `hi` (PBKDF2 with HMAC as PseudoRandom Function)
+    public static func calculate(password: String, salt: [Byte], iterations: Int, variant: HashProtocol.Type) throws -> [Byte] {
+        var salt = salt
+        salt.append(contentsOf:)(contentsOf: [0, 0, 0, 1])
+        
+        var passwordBytes = [Byte]()
+        passwordBytes.append(contentsOf: password.utf8)
+        
+        var ui = try digest(passwordBytes, data: salt, variant: variant)
+        var u1 = ui
+        
+        for _ in 0..<iterations - 1 {
+            u1 = try digest(passwordBytes, data: u1, variant: variant)
             ui = xor(ui, u1)
         }
         
