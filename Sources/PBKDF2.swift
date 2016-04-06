b3f35a034cd6667387378def4e29637fd2931845
Joannis Orlandos <joannis@orlandos.nl>
2016-04-06 14:27:41 +0200

    Made PBKDF2 Generic

--- Sources/PBKDF2.swift
+++ Sources/PBKDF2.swift
@@ -10,22 +10,22 @@ import C7
 import HMAC
 import CryptoEssentials
 
-public final class PBKDF2 {
+public final class PBKDF2<Variant: HashProtocol> {
     /// Used for applying an HMAC variant on a password and salt
-    private static func digest(password: [Byte], data: [Byte], variant: HashProtocol.Type) throws -> [Byte] {
-        return HMAC.authenticate(data, withKey: password, using: variant)
+    private static func digest(password: [Byte], data: [Byte]) throws -> [Byte] {
+        return HMAC<Variant>.authenticate(data, withKey: password)
     }
     
     /// Applies the `hi` (PBKDF2 with HMAC as PseudoRandom Function)
-    public static func calculate(password: [Byte], salt: [Byte], iterations: Int, variant: HashProtocol.Type) throws -> [Byte] {
+    public static func calculate(password: [Byte], salt: [Byte], iterations: Int) throws -> [Byte] {
         var salt = salt
         salt.append(contentsOf:)(contentsOf: [0, 0, 0, 1])
         
-        var ui = try digest(password, data: salt, variant: variant)
+        var ui = try digest(password, data: salt)
         var u1 = ui
         
         for _ in 0..<iterations - 1 {
-            u1 = try digest(password, data: u1, variant: variant)
+            u1 = try digest(password, data: u1)
             ui = xor(ui, u1)
         }
         
@@ -33,18 +33,18 @@ public final class PBKDF2 {
     }
     
     /// Applies the `hi` (PBKDF2 with HMAC as PseudoRandom Function)
-    public static func calculate(password: String, salt: [Byte], iterations: Int, variant: HashProtocol.Type) throws -> [Byte] {
+    public static func calculate(password: String, salt: [Byte], iterations: Int) throws -> [Byte] {
         var salt = salt
         salt.append(contentsOf:)(contentsOf: [0, 0, 0, 1])
         
         var passwordBytes = [Byte]()
         passwordBytes.append(contentsOf: password.utf8)
         
-        var ui = try digest(passwordBytes, data: salt, variant: variant)
+        var ui = try digest(passwordBytes, data: salt)
         var u1 = ui
         
         for _ in 0..<iterations - 1 {
-            u1 = try digest(passwordBytes, data: u1, variant: variant)
+            u1 = try digest(passwordBytes, data: u1)
             ui = xor(ui, u1)
         }
         
