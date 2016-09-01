21f29db0ffbf0a040016486ef55483b431ac3d25
Robbert Brandsma <i@robbertbrandsma.nl>
2016-09-01 18:46:47 +0200

    Swift 08-18

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "PBKDF2",
     dependencies: [
-                      .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 11)
+                      .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 13)
     ]
 )
